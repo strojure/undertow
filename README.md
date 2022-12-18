f0b2dc1c71867a58afcc2e38e76f1d60145f1352
serioga <github@sergey.trofimov.name>
2022-12-18 16:32:35 +0100

    Prepare project base

---
+++ README.md
@@ -0,0 +1,8 @@
+# undertow
+
+Clojure API to Undertow web server.
+
+[![cljdoc badge](https://cljdoc.org/badge/com.github.strojure/undertow)](https://cljdoc.org/d/com.github.strojure/undertow)
+[![Clojars Project](https://img.shields.io/clojars/v/com.github.strojure/undertow.svg)](https://clojars.org/com.github.strojure/undertow)
+
+## Motivation
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.github.strojure/undertow "1.0.0-SNAPSHOT"
+  :description " Clojure API to Undertow web server."
+  :url "https://github.com/strojure/undertow"
+  :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[io.undertow/undertow-core "2.3.0.Final"]]
+
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.11.1"]]}}
+
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]])
