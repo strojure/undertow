2c8fef05eb81ab9dc844820a7dccadac7858e621
Sergey Trofimov <sergey@trofimov.name>
2022-12-20 09:14:16 +0100

    Add function `exchange/put-headers!`

--- src/strojure/undertow/api/exchange.clj
+++ src/strojure/undertow/api/exchange.clj
@@ -2,7 +2,9 @@
   "Helper functions to work with `HttpServerExchange`."
   (:import (io.undertow.server Connectors HttpHandler HttpServerExchange)
            (io.undertow.server.session Session SessionConfig SessionManager)
-           (java.io OutputStream)))
+           (io.undertow.util HeaderMap HttpString)
+           (java.io OutputStream)
+           (java.util Collection)))
 
 (set! *warn-on-reflection* true)
 
@@ -93,5 +95,72 @@
   [^HttpServerExchange e]
   (start-blocking* e)
   (.getOutputStream e))
+
+;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+(defn- put-headers!
+  "Inserts map `headers` into mutating `header-map!`, returns `nil`.
+
+  - All keys and values in `headers` are converted to string with `str`.
+  - Sequential values are put as multiple headers.
+  - Keys from `headers` overwrite existing keys in `header-map`.
+  - Keys with `nil` value remove headers from `header-map`.
+  "
+  [header-map!, headers]
+  (reduce-kv (fn [^HeaderMap h! k v]
+               (cond
+                 (sequential? v)
+                 (doto h! (.putAll (HttpString. (str k))
+                                   ^Collection (map str v)))
+                 (some? v)
+                 (doto h! (.put (HttpString. (str k)) (str v)))
+                 :else
+                 (doto h! (.remove (HttpString. (str k))))))
+             header-map!
+             headers))
+
+(comment
+  (doto (HeaderMap.)
+    (.put (HttpString. "a") "1")
+    (.put (HttpString. "b") "2")
+    (.put (HttpString. "c") "3"))
+  ;=> #object[io.undertow.util.HeaderMap 0x3f1121c8 "{a=[1], b=[2], c=[3]}"]
+  ;             Execution time mean : 98,584711 ns
+  ;    Execution time std-deviation : 40,607351 ns
+  ;   Execution time lower quantile : 62,501697 ns ( 2,5%)
+  ;   Execution time upper quantile : 142,989464 ns (97,5%)
+  (doto (HeaderMap.)
+    (put-headers! {"a" "1" "b" "2" "c" "3"}))
+  ;=> #object[io.undertow.util.HeaderMap 0x27f9be32 "{a=[1], b=[2], c=[3]}"]
+  ;             Execution time mean : 227,497245 ns
+  ;    Execution time std-deviation : 55,560311 ns
+  ;   Execution time lower quantile : 189,682926 ns ( 2,5%)
+  ;   Execution time upper quantile : 311,745386 ns (97,5%)
+  (doto (HeaderMap.)
+    (put-headers! {"a" ["1" "2" "3"]}))
+  ;=> #object[io.undertow.util.HeaderMap 0x3fadaad "{a=[1, 2, 3]}"]
+  ;             Execution time mean : 543,457907 ns
+  ;    Execution time std-deviation : 26,917643 ns
+  ;   Execution time lower quantile : 523,367311 ns ( 2,5%)
+  ;   Execution time upper quantile : 578,557000 ns (97,5%)
+  (doto (HeaderMap.)
+    (put-headers! {"a" 1 "b" 2 "c" 3}))
+  ;=> #object[io.undertow.util.HeaderMap 0x7206ac57 "{a=[1], b=[2], c=[3]}"]
+  (doto (HeaderMap.)
+    (put-headers! {:a 1 :b 2 :c 3}))
+  ;=> #object[io.undertow.util.HeaderMap 0x15a43a2b "{:a=[1], :b=[2], :c=[3]}"]
+  (doto (HeaderMap.)
+    (put-headers! {"a" "1" "b" "2" "c" "3"})
+    (put-headers! {"c" nil}))
+  ;=> #object[io.undertow.util.HeaderMap 0x47650438 "{a=[1], b=[2]}"]
+  (doto (HeaderMap.)
+    (put-headers! {"a" ["1" "2" "3"]})
+    (put-headers! {"a" ["4" "5" "6"]}))
+  ;=> #object[io.undertow.util.HeaderMap 0x45240707 "{a=[4, 5, 6]}"]
+  (doto (HeaderMap.)
+    (put-headers! {"a" ["1" "2" "3"]})
+    (put-headers! {"a" nil}))
+  ;=> #object[io.undertow.util.HeaderMap 0x64479e67 "{}"]
+  )
 
 ;;,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
