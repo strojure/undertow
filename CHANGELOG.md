1aafc684c5149e9338d2b32bf6ebb787e84a7664
Sergey Trofimov <github@sergey.trofimov.name>
2022-12-20 11:44:28 +0100

    Release version `1.0.18-beta2`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,6 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
-## Version `1.0.15-beta1` on 2022-12-20
+## Version `1.0.18-beta2` on 2022-12-20
 
 - Implement Undertow API.
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/undertow "1.0.16-SNAPSHOT"
+(defproject com.github.strojure/undertow "1.0.18-beta2"
   :description "Clojure API to Undertow web server."
   :url "https://github.com/strojure/undertow"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
