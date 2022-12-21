2671764ee97518cc702a7e70dd9446e80e9b25ee
Sergey Trofimov <github@sergey.trofimov.name>
2022-12-21 16:44:56 +0100

    Release version `1.0.31-beta.4`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/undertow "1.0.29-SNAPSHOT"
+(defproject com.github.strojure/undertow "1.0.31-beta.4"
   :description "Clojure API to Undertow web server."
   :url "https://github.com/strojure/undertow"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
