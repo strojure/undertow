7e10fdc1d99f4fbcb6fc550ce35aa52df609aeda
serioga <github@sergey.trofimov.name>
2022-12-23 14:59:10 +0100

    Bump release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/undertow "1.0.57-beta7"
+(defproject com.github.strojure/undertow "1.0.58-SNAPSHOT"
   :description "Clojure API to Undertow web server."
   :url "https://github.com/strojure/undertow"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
