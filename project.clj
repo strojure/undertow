3496eb0ef15bd6d996a1671fec05b90246d5aa03
Sergey Trofimov <github@sergey.trofimov.name>
2023-01-17 11:20:47 +0100

    Bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/undertow "1.0.72-rc2"
+(defproject com.github.strojure/undertow "1.0.73-SNAPSHOT"
   :description "Clojure API to Undertow web server."
   :url "https://github.com/strojure/undertow"
   :license {:name "The MIT License" :url "http://opensource.org/licenses/MIT"}
