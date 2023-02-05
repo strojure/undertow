04b7806757409dbd583a3f333bb3b29a0d585804
Sergey Trofimov <github@sergey.trofimov.name>
2023-02-05 12:09:52 +0100

    Reformat Java code

--- src/strojure/undertow/websocket/WebSocketChannelListener.java
+++ src/strojure/undertow/websocket/WebSocketChannelListener.java
@@ -15,89 +15,83 @@ import java.util.Arrays;
 
 /**
  * Websocket channel listener implementation configured by Clojure map with
- * optional keys `:on-connect`, `:on-message`, `:on-close` and `:on-error`.
+ * optional keys {@code :on-connect}, {@code :on-message}, {@code :on-close} and
+ * {@code :on-error}.
  *
  * <p>Implemented in Java because Clojure's proxy is slow and does not allow to
  * call super methods.
  */
-public class WebSocketChannelListener extends AbstractReceiveListener implements WebSocketConnectionCallback {
+public class WebSocketChannelListener
+    extends AbstractReceiveListener implements WebSocketConnectionCallback
+{
   private final IFn onConnect;
   private final IFn onMessage;
   private final IFn onClose;
   private final IFn onError;
 
-  private static final Keyword k_callback = RT.keyword(null, "callback");
-  private static final Keyword k_channel = RT.keyword(null, "channel");
-  private static final Keyword k_text = RT.keyword(null, "text");
-  private static final Keyword k_data = RT.keyword(null, "data");
-  private static final Keyword k_onMessage = RT.keyword(null, "on-message");
+  private static final Keyword k_callback  = RT.keyword( null, "callback" );
+  private static final Keyword k_channel   = RT.keyword( null, "channel" );
+  private static final Keyword k_text      = RT.keyword( null, "text" );
+  private static final Keyword k_data      = RT.keyword( null, "data" );
+  private static final Keyword k_onMessage = RT.keyword( null, "on-message" );
 
-  public WebSocketChannelListener(ILookup config) {
-    this.onConnect = (IFn) config.valAt(RT.keyword(null, "on-connect"));
-    this.onMessage = (IFn) config.valAt(RT.keyword(null, "on-message"));
-    this.onClose = (IFn) config.valAt(RT.keyword(null, "on-close"));
-    this.onError = (IFn) config.valAt(RT.keyword(null, "on-error"));
+  public WebSocketChannelListener (ILookup config) {
+    this.onConnect = (IFn) config.valAt( RT.keyword( null, "on-connect" ) );
+    this.onMessage = (IFn) config.valAt( RT.keyword( null, "on-message" ) );
+    this.onClose   = (IFn) config.valAt( RT.keyword( null, "on-close" ) );
+    this.onError   = (IFn) config.valAt( RT.keyword( null, "on-error" ) );
   }
 
-  @Override
-  public void onConnect(WebSocketHttpExchange exchange,
-                        WebSocketChannel channel) {
+  @Override public void onConnect (WebSocketHttpExchange exchange,
+                                   WebSocketChannel channel) {
     if (onConnect != null) {
-      onConnect.invoke(RT.map(k_callback, RT.keyword(null, "on-connect"),
-                              RT.keyword(null, "exchange"), exchange,
-                              k_channel, channel));
+      onConnect.invoke( RT.map( k_callback, RT.keyword( null, "on-connect" ),
+                                RT.keyword( null, "exchange" ), exchange,
+                                k_channel, channel ) );
     }
   }
 
-  @Override
-  protected void onFullTextMessage(WebSocketChannel channel,
-                                   BufferedTextMessage text) throws IOException {
-    if (this.onMessage == null)
-      super.onFullTextMessage(channel, text);
-    else
-      onMessage.invoke(RT.map(k_callback, k_onMessage,
-                              k_channel, channel,
-                              k_text, text.getData()));
+  @Override protected void onFullTextMessage (WebSocketChannel channel,
+                                              BufferedTextMessage text) throws IOException {
+    if (this.onMessage != null)
+      onMessage.invoke( RT.map( k_callback, k_onMessage,
+                                k_channel, channel,
+                                k_text, text.getData() ) );
+    else super.onFullTextMessage( channel, text );
   }
 
-  @Override
-  protected void onFullBinaryMessage(WebSocketChannel channel,
-                                     BufferedBinaryMessage binary) throws IOException {
-    if (this.onMessage == null)
-      super.onFullBinaryMessage(channel, binary);
-    else {
-      @SuppressWarnings("deprecation")
+  @Override protected void onFullBinaryMessage (WebSocketChannel channel,
+                                                BufferedBinaryMessage binary) throws IOException {
+    if (this.onMessage != null) {
+      @SuppressWarnings( "deprecation" )
       Pooled<ByteBuffer[]> pooled = binary.getData();
-      byte[] buffer = WebSockets.mergeBuffers(pooled.getResource()).array();
-      byte[] data = Arrays.copyOf(buffer, buffer.length);
+      byte[] buffer = WebSockets.mergeBuffers( pooled.getResource() ).array();
+      byte[] data = Arrays.copyOf( buffer, buffer.length );
       pooled.free();
-      onMessage.invoke(RT.map(k_callback, k_onMessage,
-                              k_channel, channel,
-                              k_data, data));
+      onMessage.invoke( RT.map( k_callback, k_onMessage,
+                                k_channel, channel,
+                                k_data, data ) );
     }
+    else super.onFullBinaryMessage( channel, binary );
   }
 
-  @Override
-  protected void onCloseMessage(CloseMessage cm,
-                                WebSocketChannel channel) {
-    if (this.onError == null)
-      super.onCloseMessage(cm, channel);
-    else
-      onClose.invoke(RT.map(k_callback, RT.keyword(null, "on-close"),
-                            k_channel, channel,
-                            RT.keyword(null, "code"), cm.getCode(),
-                            RT.keyword(null, "reason"), cm.getReason()));
+  @Override protected void onCloseMessage (CloseMessage cm,
+                                           WebSocketChannel channel) {
+    if (this.onError != null)
+      onClose.invoke( RT.map( k_callback, RT.keyword( null, "on-close" ),
+                              k_channel, channel,
+                              RT.keyword( null, "code" ), cm.getCode(),
+                              RT.keyword( null, "reason" ), cm.getReason() ) );
+    else super.onCloseMessage( cm, channel );
   }
 
-  @Override
-  protected void onError(WebSocketChannel channel,
-                         Throwable error) {
-    if (this.onError == null)
-      super.onError(channel, error);
-    else
-      this.onError.invoke(RT.map(k_callback, RT.keyword(null, "on-error"),
-                                 k_channel, channel,
-                                 RT.keyword(null, "error"), error));
+  @Override protected void onError (WebSocketChannel channel,
+                                    Throwable error) {
+    if (this.onError != null)
+      this.onError.invoke( RT.map( k_callback, RT.keyword( null, "on-error" ),
+                                   k_channel, channel,
+                                   RT.keyword( null, "error" ), error ) );
+    else super.onError( channel, error );
   }
 
 }
