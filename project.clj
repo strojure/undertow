28ed0ac25ffc4a551b1c86d9595fd2ab18247b2d
Sergey Trofimov <sergey@trofimov.name>
2023-03-28 15:29:55 +0200

    (chore): bump snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/undertow "1.3.1-120"
+(defproject com.github.strojure/undertow "1.3.2-121-SNAPSHOT"
   :description "Clojure API to Undertow web server."
   :url "https://github.com/strojure/undertow"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
